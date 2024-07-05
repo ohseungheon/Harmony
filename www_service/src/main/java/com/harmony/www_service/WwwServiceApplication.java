6316193cdf2eec9a5c8d4f9464513573817d9f72
Sunny-window <96991512+Sunny-window@users.noreply.github.com>
2024-07-05 12:54:04 +0900

    프로젝트 생성

---
+++ www_service/src/main/java/com/harmony/www_service/WwwServiceApplication.java
@@ -0,0 +1,13 @@
+package com.harmony.www_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwwServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WwwServiceApplication.class, args);
+	}
+
+}
---
+++ www_service/src/test/java/com/harmony/www_service/WwwServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harmony.www_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwwServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
