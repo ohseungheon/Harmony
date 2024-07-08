68c755f8a6deee41f1689959c7694fa8d8f64742
TerRiek <ycjeong99@gmail.com>
2024-07-08 16:07:31 +0900

    memberDto,memberDao,member-mapper / 회원수정페이지 구현 완(세션은 미적용)

--- www_service/src/main/java/com/harmony/www_service/controller/Mypage1Controller.java
+++ www_service/src/main/java/com/harmony/www_service/controller/Mypage1Controller.java
@@ -1,13 +1,25 @@
 package com.harmony.www_service.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.harmony.www_service.dao.IMemberDao;
+import com.harmony.www_service.dto.MemberDto;
+
 
 @Controller
 public class Mypage1Controller {
-    
+    @Autowired
+    IMemberDao memDao;
+	
 	@RequestMapping("/info_update")
-	public String infoUpdate() {
+	public String infoUpdate(@RequestParam("mno") int mno, Model model) {
+		MemberDto member = memDao.getmemberList(mno);
+		model.addAttribute("member", member);
+		System.out.println(member);
 		return "mypage1/info_update";
 	}
 }
--- www_service/src/main/java/com/harmony/www_service/dto/MemberDto.java
+++ www_service/src/main/java/com/harmony/www_service/dto/MemberDto.java
@@ -14,6 +14,7 @@ public class MemberDto {
 
 	private int mno;
 	private String username;
+	private String password;
 	private String nickName;
 	private String addr;
 	private LocalDate birth;
