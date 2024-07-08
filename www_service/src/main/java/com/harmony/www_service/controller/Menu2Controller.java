f393df6640b978ee6a2b506e64dcc17886613308
Sunny-window <96991512+Sunny-window@users.noreply.github.com>
2024-07-08 17:54:41 +0900

    메뉴 추가 요청 구현 중

--- www_service/src/main/java/com/harmony/www_service/controller/Menu2Controller.java
+++ www_service/src/main/java/com/harmony/www_service/controller/Menu2Controller.java
@@ -1,7 +1,12 @@
 package com.harmony.www_service.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.harmony.www_service.dto.MenuWithFile;
+
 
 @Controller
 @RequestMapping("/menu2")
@@ -10,8 +15,14 @@ public class Menu2Controller {
     
     @RequestMapping("/addForm")
     public String addRequestForm() {
-
         
         return "menu2/menu_request";
     }
+
+    @PostMapping("/add")
+    public String postMethodName(@RequestBody MenuWithFile reqMenu) {
+        
+        return "redirect:/"; // menu all list 로 이동시켜야 됨 
+    }
+    
 }
