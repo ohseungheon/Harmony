e0cf02ab3d86bcfec273a3f0188d31ea1822d7ab
DESKTOP-4DR04SQ\GG <ujlee0821@gmail.com>
2024-07-08 17:53:04 +0900

    재료 등록 페이지 작성중

--- www_service/src/main/java/com/harmony/www_service/controller/ManagerController.java
+++ www_service/src/main/java/com/harmony/www_service/controller/ManagerController.java
@@ -1,5 +1,13 @@
 package com.harmony.www_service.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ManagerController {
     
+	@RequestMapping("/ingredient_regist")
+	public String ingredientRegist() {
+		return "manager/ingredients_regist";
+	}
 }
