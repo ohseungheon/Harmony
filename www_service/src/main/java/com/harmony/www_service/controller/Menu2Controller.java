5b0c63700f2c3a2bfba30ba63af27191b0ba7f11
Sunny-window <96991512+Sunny-window@users.noreply.github.com>
2024-07-08 14:08:40 +0900

    menu2 컨트롤러 설정

--- www_service/src/main/java/com/harmony/www_service/controller/Menu2Controller.java
+++ www_service/src/main/java/com/harmony/www_service/controller/Menu2Controller.java
@@ -1,5 +1,13 @@
 package com.harmony.www_service.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/menu2")
 public class Menu2Controller {
-    
+    @RequestMapping("/addForm")
+    public String addRequestForm() {
+        return "menu2/menu_request";
+    }
 }
---
+++ www_service/src/main/java/com/harmony/www_service/restcontroller/Menu2RestCon.java
@@ -0,0 +1,12 @@
+package com.harmony.www_service.restcontroller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/menu2-api")
+public class Menu2RestCon {
+
+}
