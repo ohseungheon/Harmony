5232705cd1c3e895b309c1f0300fc56100254566
Kim TaeJun <1000pgm@gmail.com>
2024-07-09 09:09:48 +0900

    로그인, 회원가입 구현

--- www_service/src/main/java/com/harmony/www_service/dao/LoginDao.java
+++ www_service/src/main/java/com/harmony/www_service/dao/LoginDao.java
@@ -2,11 +2,20 @@ package com.harmony.www_service.dao;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.harmony.www_service.dto.MemberDto;
 import com.harmony.www_service.dto.UserDto;
+import com.harmony.www_service.dto.UserMemberDto;
 
 @Mapper
 public interface LoginDao {
 
+	//로그인
 	public UserDto loginCheck(UserDto userDto);
+	
+	//회원가입 user
+	public int registUser(UserMemberDto userMemberDto);
+	
+	//회원가입 member
+	public int registMember(UserMemberDto userMemberDto);
 	
 }
--- www_service/src/main/java/com/harmony/www_service/dto/UserMemberDto.java
+++ www_service/src/main/java/com/harmony/www_service/dto/UserMemberDto.java
@@ -1,17 +1,18 @@
 package com.harmony.www_service.dto;
 
-
 import java.time.LocalDate;
 
 import lombok.Data;
 
 @Data
-public class MemberDto {
-
-	private int mno;
+public class UserMemberDto {
+	
 	private String username;
+	private String password;
+	private String role;
 	private String nickName;
 	private String addr;
 	private LocalDate birth;
 	private String phone;
+
 }
