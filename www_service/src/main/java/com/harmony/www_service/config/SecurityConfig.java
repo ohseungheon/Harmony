5a6c48c7ecb95524f713c739b5821d30dad31cad
김태준 <asxowns@naver.com>
2024-07-09 20:20:22 +0900

    로그인시 아이디 비밀번호 틀리면 틀렷다고 alert창 띄움

--- www_service/src/main/java/com/harmony/www_service/config/SecurityConfig.java
+++ www_service/src/main/java/com/harmony/www_service/config/SecurityConfig.java
@@ -1,21 +1,32 @@
 package com.harmony.www_service.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	
+	@Autowired
+	private CustomAuthenticationFailureHandler customAuthenticationFailureHandler;
 
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 
 		return new BCryptPasswordEncoder();
 	}
+	
+//	@Bean
+//	public UserDetailsService userDetailsService() {
+//		
+//		return new CustomUserDetailsService();
+//	}
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -31,6 +42,7 @@ public class SecurityConfig {
 				.formLogin((auth) -> auth
 				.loginPage("/login_page")
 				.loginProcessingUrl("/loginProc")
+				.failureHandler(customAuthenticationFailureHandler)
 				.permitAll()
 				);
 		
--- www_service/src/main/java/com/harmony/www_service/controller/LoginController.java
+++ www_service/src/main/java/com/harmony/www_service/controller/LoginController.java
@@ -1,11 +1,15 @@
 package com.harmony.www_service.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.harmony.www_service.dto.UserDto;
 import com.harmony.www_service.service.LoginService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class LoginController {
@@ -28,13 +32,16 @@ public class LoginController {
 	@RequestMapping("/loginProc")
 	public String loginProc(UserDto userDto) {
 		
-		UserDto user = loginService.loginCheckService(userDto);
-		
-		if(user != null) {
-			
-		}
 		
 		return "/";
+	}
+	
+	@PostMapping("/clearErrorMessage")
+	public ResponseEntity<Void> clearErrorMessage(HttpSession session) {
+		
+		session.removeAttribute("errormsg");
+		
+		return ResponseEntity.ok().build();
 	}
 
 
