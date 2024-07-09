331745914c6581fe9f4818133217cc7088c813cc
KimDongHwi <bsw0825@naver.com>
2024-07-09 10:42:13 +0900

    개인 환경 디비 접속설정, 메뉴 리스트 개별 선택시 recipe_list로 mcode 전송 구현.

--- www_service/src/main/java/com/harmony/www_service/controller/MenuAllController.java
+++ www_service/src/main/java/com/harmony/www_service/controller/MenuAllController.java
@@ -1,7 +1,9 @@
 package com.harmony.www_service.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MenuAllController {
@@ -11,4 +13,14 @@ public class MenuAllController {
 
         return "menu_all/menu_all_list";
     }
+
+    @GetMapping("/menu_all/recipe_list")
+    public String recipe_list(@RequestParam("mcode") int mcode, Model model){
+
+        model.addAttribute("mcode", mcode);
+        return "menu_all/recipe_list";
+    }
+
+
+
 }
