1098fc493403a35284ba0fcc6c8338a33f33310b
Park Saehee <saehee0427@gmail.com>
2024-07-09 09:32:50 +0900

    menu1 controller,mapper 수정 및 dto파일 생성

--- www_service/src/main/java/com/harmony/www_service/dao/Menu1Dao.java
+++ www_service/src/main/java/com/harmony/www_service/dao/Menu1Dao.java
@@ -1,8 +1,12 @@
 package com.harmony.www_service.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.harmony.www_service.dto.IngredientDto;
 
 @Mapper
 public interface Menu1Dao {
-
+	List<IngredientDto> showFridgeIngredient();
 }
