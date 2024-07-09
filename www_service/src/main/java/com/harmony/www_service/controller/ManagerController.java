d0a421bad30ad46f50cf2b2629af4d826805a76d
ej <ujlee0821@gmail.com>
2024-07-09 23:34:25 +0900

    file만 하면 등록됨 아마도 ㅡㅡ

--- www_service/src/main/java/com/harmony/www_service/controller/ManagerController.java
+++ www_service/src/main/java/com/harmony/www_service/controller/ManagerController.java
@@ -1,13 +1,15 @@
 package com.harmony.www_service.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.harmony.www_service.dto.IngredientDto;
+import com.harmony.www_service.dto.IngredientDtoWithFile;
 import com.harmony.www_service.service.IngredientService_dally;
 
 @Controller
@@ -22,13 +24,22 @@ public class ManagerController {
 		
 		return "manager/ingredients_regist";
 	}
+	
 	// 재료 등록 기능
 	@PostMapping("/do_ingredients_regist")
-	public String ingredientRegist(IngredientDto iDto, Model model) {
+	public String ingredientRegist(IngredientDto iDto) {
 		System.out.println("--------post--------");
-		iService.save(iDto);
-		model.addAttribute("msg", "등록되었습니다!");
-		return "redirect: manager/ingredients_regist";
+		
+		IngredientDtoWithFile iDtoFile = new IngredientDtoWithFile();
+		
+		iDtoFile.setName(iDto.getName());
+		iDtoFile.setCategory(iDto.getCategory());
+		iDtoFile.setTip(iDto.getTip());
+		iDtoFile.setType(iDto.getType());
+		//iDtoFile.set
+		
+		iService.save(iDtoFile);
+		return "redirect: manager/ingredients_list";
 	}
 	
 //	@RequestMapping("ingredient_update")
@@ -36,5 +47,19 @@ public class ManagerController {
 //		System.out.println("--------update--------");
 //		
 //	}
+	
+	// 재료 리스트 페이지
+	@RequestMapping("/ingredients_list")
+	public String goList() {
+		return "manager/ingredients_list";
+	}
+	
+	// 재료 리스트 기능 구현
+	@RequestMapping("do_ingredients_list")
+	public List<IngredientDto> getList(@RequestParam("category") String category){
+		List<IngredientDto> iList = iService.showList(category);
+		return iList;
+	}
+	
 	
 }
--- www_service/src/main/java/com/harmony/www_service/service/IngredientService_dally.java
+++ www_service/src/main/java/com/harmony/www_service/service/IngredientService_dally.java
@@ -1,10 +1,13 @@
 package com.harmony.www_service.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.harmony.www_service.dao.IIngredientDao;
 import com.harmony.www_service.dto.IngredientDto;
+import com.harmony.www_service.dto.IngredientDtoWithFile;
 
 @Service
 public class IngredientService_dally {
@@ -12,11 +15,15 @@ public class IngredientService_dally {
 	@Autowired
 	IIngredientDao iDao;
 	
-	public void save(IngredientDto iDto) {
-		
-		if(iDto.getIcode() != -1) {
-			iDao.registIngredient(iDto);
+	public void save(IngredientDtoWithFile iDtoFile) {
+		if(iDtoFile.getIcode() != -1) {
+			iDao.registIngredient(iDtoFile);
 		}
+	}
+	
+	public List<IngredientDto> showList(String category){
+		List<IngredientDto> iList = iDao.getList(category);
 		
+		return iList;
 	}
 }
