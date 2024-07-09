3b6b31d4d7e7c93892280e0224df2222cac58f07
김태준 <asxowns@naver.com>
2024-07-09 16:37:58 +0900

    섹션 1000px; , margin:0 auto;

--- www_service/src/main/java/com/harmony/www_service/controller/LoginController.java
+++ www_service/src/main/java/com/harmony/www_service/controller/LoginController.java
@@ -5,25 +5,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
-	
+
 	@RequestMapping("/login_page")
 	public String loginForm() {
-		
+
 		return "main/login_page";
 	}
-	
+
 	@RequestMapping("/regist_page")
 	public String registForm() {
-		
+
 		return "main/regist_page";
 	}
-	
-	//@PostMapping("/loginProc")
-	public String loginProc() {
-		
-		
-		
-		return "/";
-	}
+
 
 }
