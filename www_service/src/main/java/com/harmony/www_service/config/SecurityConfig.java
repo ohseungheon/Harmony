03aded14b7b5e6e92c7b20fd5a4acc9047cb5996
김태준 <asxowns@naver.com>
2024-07-12 12:30:13 +0900

    menu_all 시큐리티컨피그 인가

--- www_service/src/main/java/com/harmony/www_service/config/SecurityConfig.java
+++ www_service/src/main/java/com/harmony/www_service/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
 						.requestMatchers("/", "/login_page", "/regist_page", "/doLogin", "/doRegist", "/loginProc").permitAll()
 						.requestMatchers("/src/**", "/main/**","/resources/**", "/static/**", "/img/**", "/json/**").permitAll()
 						.requestMatchers("/api/**").permitAll()
+						.requestMatchers("/menu_all/**").permitAll()
 						.requestMatchers("/admin").hasRole("ADMIN")
 						.requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
 						.anyRequest().authenticated()
