0ad1154ae965165c795198279d215c59e0d65b98
김태준 <asxowns@naver.com>
2024-07-13 18:26:22 +0900

    레시피 등록 하는중 ㅜ

--- www_service/src/main/java/com/harmony/www_service/controller/Mypage2Controller.java
+++ www_service/src/main/java/com/harmony/www_service/controller/Mypage2Controller.java
@@ -1,17 +1,29 @@
 package com.harmony.www_service.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.harmony.www_service.dao.RecipeMenuDao;
+import com.harmony.www_service.dto.MemberDto;
 import com.harmony.www_service.dto.MenuDto;
+import com.harmony.www_service.dto.RecipeDto;
+import com.harmony.www_service.dto.RecipeIngredientDto;
+import com.harmony.www_service.dto.RecipeOrderDto;
+import com.harmony.www_service.dto.RecipeTagDto;
+import com.harmony.www_service.service.LoginService;
 import com.harmony.www_service.service.MyRecipeService;
+import com.harmony.www_service.util.FileUploadUtil;
 
 @Controller
 @RequestMapping("/my/recipe")
@@ -19,6 +31,9 @@ public class Mypage2Controller {
 	
 	@Autowired
 	private MyRecipeService service;
+	
+	@Autowired
+	private LoginService loginService;
 	
 	@Autowired
 	private RecipeMenuDao menuDao;
@@ -39,25 +54,74 @@ public class Mypage2Controller {
 		return "mypage2/recipe_regist";
 	}
 	
-	//@PostMapping("/regist")
-//	public String recipeRegist(@RequestParam("mcode") int mcode,
-//			@RequestParam("recipeName") String recipeName,
-//			@RequestParam("introduce") String introduce,
-//			@RequestParam("url") String url,
-//			@RequestParam("category") String category,
-//			@RequestParam("portions") String protions,
-//			@RequestParam("icode") int icode,
-//			@RequestParam("amount") int amount
-//			) {
+	@PostMapping("/regist")
+	@Transactional
+	public String recipeRegist(@RequestParam("mcode") int mcode,
+			@RequestParam("recipeName") String recipeName,
+			@RequestParam("introduce") String introduce,
+			@RequestParam("url") String url,
+			@RequestParam("category") String category,
+			@RequestParam("portions") int portions,
+			@RequestParam("icode") int icode,
+			@RequestParam("amount") int amount,
+			@RequestParam("type") String type,
+			@RequestParam("orderNum") int orderNum,
+			@RequestParam("content") String orderContent,
+			@RequestPart("cookingImg") MultipartFile cookingImg,
+			@RequestParam("content") String tagContent)
+			{
 		
-		//service.registMyRecipeService(recipeDto);
-		//service.registMyRecipeIngredientService(recipeIngredientDto);
-		//service.registMyRecipeOrderService(recipeOrderDto);
-		//service.registMyRecipeTagService(recipeTagDto);
+		String cookingImgName = "";
+		
+
+		try {
+			if (cookingImg != null && !cookingImg.isEmpty()) {
+				cookingImgName = FileUploadUtil.saveFile(cookingImg.getOriginalFilename(), cookingImg);
+	        }
+
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+		
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+		MemberDto mno = loginService.getMnoByUsernameService(username);
+		System.out.println(mno.getMno());
+		
+		// recipe 테이블 등록
+		RecipeDto recipeDto = new RecipeDto();
+		recipeDto.setMcode(mcode);
+		recipeDto.setMno(mno.getMno());
+		recipeDto.setRecipeName(recipeName);
+		recipeDto.setIntroduce(introduce);
+		recipeDto.setUrl(url);
+		recipeDto.setCategory(category);
+		recipeDto.setPortions(portions);
+		service.registMyRecipeService(recipeDto);
+		
+		int rcode = recipeDto.getRcode();
+		
+		// recipe_ingredient 테이블 등록
+		RecipeIngredientDto recipeIngredientDto = new RecipeIngredientDto();
+		recipeIngredientDto.setRcode(rcode);
+		recipeIngredientDto.setIcode(icode);
+		recipeIngredientDto.setAmount(amount);
+		service.registMyRecipeIngredientService(recipeIngredientDto);
+		
+		RecipeOrderDto recipeOrderDto = new RecipeOrderDto();
+		recipeOrderDto.setRcode(rcode);
+		recipeOrderDto.setContent(orderContent);
+		recipeOrderDto.setOrderNum(orderNum);
+		recipeOrderDto.setCookingimg(cookingImgName);
+		service.registMyRecipeOrderService(recipeOrderDto);
+		
+		RecipeTagDto recipeTagDto = new RecipeTagDto();
+		recipeTagDto.setRcode(rcode);
+		recipeTagDto.setConetent(tagContent);
+		service.registMyRecipeTagService(recipeTagDto);
 		
 		
-		//return "redirect:mypage2/my_recipe_list";
-	//}
+		return "redirect:mypage2/my_recipe_list";
+	}
 	
 	
 	
