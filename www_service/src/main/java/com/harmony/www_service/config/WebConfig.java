241fa58df8ca45a4bc489a703d68150486661084
Sunny-window <96991512+Sunny-window@users.noreply.github.com>
2024-07-14 16:13:22 +0900

    메뉴 이미지 위치 조정, 냉부 시즌2 기능 완

--- www_service/src/main/java/com/harmony/www_service/config/WebConfig.java
+++ www_service/src/main/java/com/harmony/www_service/config/WebConfig.java
@@ -10,20 +10,17 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Value("${file.upload-dir}")
     private String uploadDir;
-    
+
     @Value("${file.ingredients-dir}")
     private String ingredientsDir;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:" + uploadDir + "/"); 
-       
-        
-        registry.addResourceHandler("/img/ingredient/**")	// 코드상에서 이미지파일을 불러오는 가상공간
-                .addResourceLocations("file:" + ingredientsDir + "/");	// 실제로 파일이 저장되는 공간
-    }
-    
-    
-}
+                .addResourceLocations("file:" + uploadDir + "/");
 
+        registry.addResourceHandler("/img/ingredient/**") // 코드상에서 이미지파일을 불러오는 가상공간
+                .addResourceLocations("file:" + ingredientsDir + "/"); // 실제로 파일이 저장되는 공간
+    }
+
+}
