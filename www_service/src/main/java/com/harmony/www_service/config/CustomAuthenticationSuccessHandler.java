94ea1ebdffda5f67b2cd4d3e2a7fb03fe59e8ff2
Sunny-window <96991512+Sunny-window@users.noreply.github.com>
2024-07-19 18:41:59 +0900

    시큐리티 로그인 성공 시 페이지이동 100% 구현 완료

--- www_service/src/main/java/com/harmony/www_service/config/CustomAuthenticationSuccessHandler.java
+++ www_service/src/main/java/com/harmony/www_service/config/CustomAuthenticationSuccessHandler.java
@@ -1,24 +1,47 @@
 package com.harmony.www_service.config;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Component;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
 @Component
-public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+public class CustomAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+
+    private RequestCache requestCache = new HttpSessionRequestCache();
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        String refererUrl = request.getHeader("Referer");
-        if (refererUrl != null) {
-            response.sendRedirect(refererUrl);
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+            Authentication authentication) throws IOException, ServletException {
+        SavedRequest savedRequest = requestCache.getRequest(request, response);
+        HttpSession session = request.getSession(false);
+
+        if (savedRequest != null) {
+            // 원래 요청한 URL로 리다이렉트 (인증이 필요해서 자동으로 로그인 페이지로 이동됐을 때)
+            String targetUrl = savedRequest.getRedirectUrl();
+            getRedirectStrategy().sendRedirect(request, response, targetUrl);
         } else {
+            // 로그인 페이지에 요청이 왔을 때 저장된 session의 referer 헤더값을 사용하여 리다이렉트할 URL 설정
+            // (메인이 아닌 페이지에서 직접 로그인 페이지로 이동해서 로그인했을 때)
+            if (session != null) {
+                String prevPage = (String) session.getAttribute("prevPage");
+                if (prevPage != null) {
+                    session.removeAttribute("prevPage");
+                    System.out.println(prevPage);
+                    System.out.println(prevPage);
+                    response.sendRedirect(prevPage);
+                    return;
+                }
+            }
             response.sendRedirect("/");
         }
     }
--- www_service/src/main/java/com/harmony/www_service/controller/LoginController.java
+++ www_service/src/main/java/com/harmony/www_service/controller/LoginController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.harmony.www_service.dto.UserDto;
 import com.harmony.www_service.service.LoginService;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class LoginController {
 
@@ -17,12 +20,16 @@ public class LoginController {
 	private LoginService loginService;
 
 	@GetMapping("/login_page")
-	public String loginForm(@RequestParam(name = "error", required = false) Integer error, Model model) {
+	public String loginForm(@RequestParam(name = "error", required = false) Integer error, Model model, HttpServletRequest request, HttpSession session) {
 		if (error != null && error == 1) {
 			model.addAttribute("err_msg", "아이디 또는 비밀번호가 틀렸습니다.");
 			return "main/login_page";
 		}
-
+		// 로그인 성공 시 로그인 페이지로 이동 전의 페이지로 redirect 해주기 위한 이전 페이지 url 저장
+		String referer = request.getHeader("Referer");
+        if (referer != null && !referer.contains("/login_page")) {
+            session.setAttribute("prevPage", referer);
+        }
 		return "main/login_page";
 	}
 
