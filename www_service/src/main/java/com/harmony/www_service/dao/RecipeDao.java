46677138d24e6ae7e4a45710d98d5793b6ece139
김태준 <asxowns@naver.com>
2024-07-22 20:28:33 +0900

    레시피 디테일, 태그추가, 디테일페이지 들어가면 view +1, 좋아요 개수 표시

--- www_service/src/main/java/com/harmony/www_service/dao/RecipeDao.java
+++ www_service/src/main/java/com/harmony/www_service/dao/RecipeDao.java
@@ -5,8 +5,9 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
-import com.harmony.www_service.dto.MenuDto;
+import com.harmony.www_service.dto.PopularRecipeDto;
 import com.harmony.www_service.dto.RecipeDto;
+import com.harmony.www_service.dto.RecipeGetTagDto;
 import com.harmony.www_service.dto.RecipeIngredientDto;
 import com.harmony.www_service.dto.RecipeOrderDto;
 
@@ -29,4 +30,13 @@ public interface RecipeDao {
     List<RecipeDto> getFilteredRecipes(@Param("category") List<String> category, @Param("ingredient") List<Integer> ingredient);
 
     List<RecipeDto> searchRecipe(String term);
+    
+//    레시피 태그 가져오기
+    RecipeGetTagDto recipeGetTag(@Param("rcode") int rcode);
+    
+//    레시피 디테일 들어갈때마다 view +1
+    RecipeDto recipeViewCount(@Param("rcode") int rcode);
+    
+//    레시피 디테일 좋아요수
+    PopularRecipeDto recipeRecommendCount(@Param("rcode") int rcode);
 }
---
+++ www_service/src/main/java/com/harmony/www_service/dto/RecipeGetTagDto.java
@@ -0,0 +1,11 @@
+package com.harmony.www_service.dto;
+
+import lombok.Data;
+
+@Data
+public class RecipeGetTagDto {
+	
+	private int rcode;
+	private String tagContent;
+
+}
