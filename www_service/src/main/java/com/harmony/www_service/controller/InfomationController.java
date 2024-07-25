1ef08a097adb3634ce1f20edba7e3b920adf182d
Sunny-window <96991512+Sunny-window@users.noreply.github.com>
2024-07-25 10:35:48 +0900

    game controller -> info controller

--- www_service/src/main/java/com/harmony/www_service/controller/InfomationController.java
+++ www_service/src/main/java/com/harmony/www_service/controller/InfomationController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/game")
-public class GameController {
+@RequestMapping("/info")
+public class InfomationController {
     @RequestMapping("/main")
     public String main(){
-        // return "game/main";
-        return "sub/ready_for_service";
+        return "info/main";
+        // return "sub/ready_for_service";
     }
 }
