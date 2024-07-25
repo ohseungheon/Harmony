f88611c6bd69960acc54f2c982bee4a6bb2f0cde
변지은 <ster137@naver.com>
2024-07-25 18:07:19 +0900

    마이페이지css수정 / 뭐하지-공공데이터 가져오기 중(act-)

--- www_service/src/main/java/com/harmony/www_service/controller/ActivityController.java
+++ www_service/src/main/java/com/harmony/www_service/controller/ActivityController.java
@@ -1,14 +1,26 @@
 package com.harmony.www_service.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.harmony.www_service.dao.IActDao;
+import com.harmony.www_service.dto.ActDto;
 
 @Controller
 @RequestMapping("/act")
 public class ActivityController {
-    @RequestMapping("/main")
-    public String main(){
-        return "act/main";
+    @Autowired
+    IActDao actDao;
+	
+	@RequestMapping("/main")
+    public String main(Model model){
+        List<ActDto> act = actDao.getActList();
+        model.addAttribute("act", act);
+    	return "act/main";
         // return "sub/ready_for_service";
     }
 }
