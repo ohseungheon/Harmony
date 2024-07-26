a1bcc53ae53327572d99a4340ab456630f0c9bae
변지은 <ster137@naver.com>
2024-07-26 15:25:26 +0900

    재료등롯, 수정 완완 / map 데리고 오기중

--- www_service/src/main/java/com/harmony/www_service/dto/FridgeIngredientDto.java
+++ www_service/src/main/java/com/harmony/www_service/dto/FridgeIngredientDto.java
@@ -13,13 +13,14 @@ public class FridgeIngredientDto {
 	private int fcode; //냉장고재료코드
 	private int mno;
 	private int icode; //재료코드
+	private String keeptype; //보관상태 
+	private String state; //재료상태 
 	private int amount; //재료수량
+	private String type; //단위구분
 	private LocalDate deadline; //유통기한
 	private String memo; //비고란
-	private String keeptype; //보관상태 
 	
 	private String name; //재료이름
-	private String type; //단위구분
 	private String tip;
 	private String imgUrl;
 	private String category; 
