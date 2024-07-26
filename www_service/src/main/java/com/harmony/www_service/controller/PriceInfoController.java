54751505c7aace98fa845b0cbbd3c5365a6a4790
Sunny-window <96991512+Sunny-window@users.noreply.github.com>
2024-07-26 09:16:57 +0900

    농축수산물·가공식품 가격 정보 페이지&컨트롤러 추가

---
+++ www_service/src/main/java/com/harmony/www_service/controller/PriceInfoController.java
@@ -0,0 +1,15 @@
+package com.harmony.www_service.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/price_info")
+public class PriceInfoController {
+    
+    @RequestMapping("/main")
+    public String main(){
+        return "price_info/main";
+    }
+    
+}
