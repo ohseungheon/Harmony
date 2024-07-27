472d66eb3c26646fb4aeb4e440368538c6b4b85a
Sunny-window <sunnywindow0203@gmail.com>
2024-07-28 06:03:31 +0900

    특정 품목 데이터, 부류별 소매 가격 데이터 구분해서 출력 테스트 완료

--- www_service/src/main/java/com/harmony/www_service/restcontroller/ProxyController.java
+++ www_service/src/main/java/com/harmony/www_service/restcontroller/ProxyController.java
@@ -1,17 +1,22 @@
 package com.harmony.www_service.restcontroller;
 
-import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.io.buffer.DataBufferLimitException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import jakarta.servlet.http.HttpServletRequest;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/proxy")
 public class ProxyController {
+
+    private String serviceKey = "69bb1f84-ae0c-4352-a558-023aaa11e1ae";
+    private String serviceID = "4619";
 
     private final WebClient webClient;
 
@@ -21,7 +26,9 @@ public class ProxyController {
 
     @GetMapping("/rural-experience-activities/**")
     public Mono<String> proxyRequest(HttpServletRequest request) {
-        String uri = "http://211.237.50.150:7080" + request.getRequestURI().replace("/proxy/rural-experience-activities", "");
+        String uri = "http://211.237.50.150:7080"
+                + request.getRequestURI().replace("/proxy/rural-experience-activities", "");
+
         return webClient.get()
                 .uri(uri)
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
@@ -29,18 +36,48 @@ public class ProxyController {
                 .bodyToMono(String.class);
     }
 
-    @GetMapping("/retail-price/**")
-    public Mono<String> retail_price(HttpServletRequest request) {
-        // String uri = "http://www.kamis.or.kr/?p_cert_key=69bb1f84-ae0c-4352-a558-023aaa11e1ae&p_cert_id=4619&p_returntype=json&p_productclscode=01&" + request.getRequestURI().replace("/proxy/retail-price/", "");
-        String uri = "https://www.kamis.or.kr/service/price/xml.do?action=periodProductList&p_productclscode=02&";
-        uri += request.getRequestURI().replace("/proxy/retail-price/", "");
-        uri +=  "&p_cert_key=69bb1f84-ae0c-4352-a558-023aaa11e1ae&p_cert_id=4619&p_returntype=json";
+    @GetMapping("/retail-product-price/**")
+    public Mono<String> retail_product_price(HttpServletRequest request) {
+        String uri = "https://www.kamis.or.kr/service/price/xml.do"
+                + "?action=periodProductList&p_productclscode=01&";
+        uri += request.getRequestURI().replace("/proxy/retail-product-price/", "");
+        uri += "&p_cert_key=" + serviceKey
+                + "&p_cert_id=" + serviceID
+                + "&p_returntype=json";
 
         System.out.println(uri);
+
         return webClient.get()
                 .uri(uri)
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .retrieve()
-                .bodyToMono(String.class);
+                .bodyToFlux(String.class)
+                .reduce("", (acc, chunk) -> acc + chunk)
+                .onErrorResume(DataBufferLimitException.class, ex -> {
+                    System.err.println("DataBufferLimitException: " + ex.getMessage());
+                    return Mono.just("Error: Data buffer limit exceeded");
+                });
+    }
+
+    @GetMapping("/retail-price/**")
+    public Mono<String> retail_price(HttpServletRequest request) {
+        String uri = "https://www.kamis.or.kr/service/price/xml.do?action=dailyPriceByCategoryList&p_product_cls_code=01&"
+                + request.getRequestURI().replace("/proxy/retail-price/", "");
+        uri += "&p_cert_key=" + serviceKey
+                + "&p_cert_id=" + serviceID
+                + "&p_returntype=json";
+
+        System.out.println(uri);
+
+        return webClient.get()
+                .uri(uri)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .retrieve()
+                .bodyToFlux(String.class)
+                .reduce("", (acc, chunk) -> acc + chunk)
+                .onErrorResume(DataBufferLimitException.class, ex -> {
+                    System.err.println("DataBufferLimitException: " + ex.getMessage());
+                    return Mono.just("Error: Data buffer limit exceeded");
+                });
     }
 }
