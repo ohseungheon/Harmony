75dab51f1c5f770052e7d77465cfd4c3d924084d
Sunny-window <sunnywindow0203@gmail.com>
2024-07-28 04:47:39 +0900

    농축수산 소매 데이터 api 테스트 요청 성공

--- www_service/src/main/java/com/harmony/www_service/restcontroller/ProxyController.java
+++ www_service/src/main/java/com/harmony/www_service/restcontroller/ProxyController.java
@@ -1,4 +1,4 @@
-package com.harmony.www_service.controller;
+package com.harmony.www_service.restcontroller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpHeaders;
@@ -22,6 +22,21 @@ public class ProxyController {
     @GetMapping("/rural-experience-activities/**")
     public Mono<String> proxyRequest(HttpServletRequest request) {
         String uri = "http://211.237.50.150:7080" + request.getRequestURI().replace("/proxy/rural-experience-activities", "");
+        return webClient.get()
+                .uri(uri)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    @GetMapping("/retail-price/**")
+    public Mono<String> retail_price(HttpServletRequest request) {
+        // String uri = "http://www.kamis.or.kr/?p_cert_key=69bb1f84-ae0c-4352-a558-023aaa11e1ae&p_cert_id=4619&p_returntype=json&p_productclscode=01&" + request.getRequestURI().replace("/proxy/retail-price/", "");
+        String uri = "https://www.kamis.or.kr/service/price/xml.do?action=periodProductList&p_productclscode=02&";
+        uri += request.getRequestURI().replace("/proxy/retail-price/", "");
+        uri +=  "&p_cert_key=69bb1f84-ae0c-4352-a558-023aaa11e1ae&p_cert_id=4619&p_returntype=json";
+
+        System.out.println(uri);
         return webClient.get()
                 .uri(uri)
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
